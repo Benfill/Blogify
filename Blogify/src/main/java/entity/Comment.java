ad79d9dbe5d6d6ca5cf5d109fea025c7f59b3b9c
Anass Benfillous <anasse.benfillous@gmail.com>
2024-10-08 15:42:38 +0100

    update comment entity

--- Blogify/src/main/java/entity/Comment.java
+++ Blogify/src/main/java/entity/Comment.java
@@ -2,14 +2,43 @@ package entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import enums.CommentStatus;
 
+@Entity
+@Table(name = "comments")
 public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+
+	@Column(name = "content", nullable = false)
 	private String content;
+
+	@Column(name = "creation_date")
 	private LocalDate creationDate;
+
+	@Enumerated(EnumType.STRING) // Maps enum as a string to the database.
+	@Column(name = "comment_status", nullable = true)
 	private CommentStatus commentStatus;
+
+	@ManyToOne(fetch = FetchType.LAZY) // Specifies a many-to-one relationship with the Article entity.
+	@JoinColumn(name = "article") // Column name in the comments table that refers to the article.
 	private Article article;
+
+	@ManyToOne(fetch = FetchType.LAZY) // Specifies a many-to-one relationship with the Article entity.
+	@JoinColumn(name = "user_id") // Column name in the comments table that refers to the user
 	private User user;
 
 	public int getId() {
