119bdf23ed673efe2e6a691ca0548a05044280b1
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2024-10-09 09:08:44 +0100

    update user entity

--- Blogify/src/main/java/entity/User.java
+++ Blogify/src/main/java/entity/User.java
@@ -94,4 +94,12 @@ public class User {
     public void setRole(UserRole role) {
         this.role = role;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
