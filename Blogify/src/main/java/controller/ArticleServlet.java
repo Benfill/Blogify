1ab4c4dd3b8953d9fb28ddae561a94535cdf6c31
Anass Benfillous <anasse.benfillous@gmail.com>
2024-10-09 11:48:40 +0100

    handle article show detail request

--- Blogify/src/main/java/controller/ArticleServlet.java
+++ Blogify/src/main/java/controller/ArticleServlet.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -11,75 +10,85 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import model.ArticleModel;
+import service.impl.ArticleServiceImpl;
+
 public class ArticleServlet extends HttpServlet {
-    
 
+	private static final Logger logger = LoggerFactory.getLogger(ArticleServlet.class);
+	private ArticleServiceImpl articleService;
 
-    private static final Logger logger = LoggerFactory.getLogger(ArticleServlet.class);
+	@Override
+	public void init() throws ServletException {
+		articleService = new ArticleServiceImpl();
+	}
 
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String path = req.getPathInfo();
 
-    @Override
-    public void init() throws ServletException {
-      
-    }
+		if (path != null) {
+			path = path.substring(1, path.length());
+			ArticleModel model = articleService.getArticleById(path);
+			if (model == null || model.getArticle() == null) {
+				this.getServletContext().getRequestDispatcher("/views/article/error.jsp").forward(req, res);
+				return;
+			}
+			req.setAttribute("article", model.getArticle());
+			this.getServletContext().getRequestDispatcher("/views/article/detail.jsp").forward(req, res);
+		} else
+			this.getServletContext().getRequestDispatcher("/views/article/index.jsp").forward(req, res);
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher("/views/article/index.jsp").forward(req, res);
+		// String action = req.getParameter("action");
+		// if (action == null || action.isEmpty()) {
+		// index(req, res);
+		// } else if ("add".equalsIgnoreCase(action)) {
+		// create(req, res);
+		// } else if ("list".equalsIgnoreCase(action)) {
+		// index(req, res);
+		// } else if ("edit".equalsIgnoreCase(action)) {
+		// edit(req,res);
+		// }
+	}
 
-        //    String action = req.getParameter("action");
-        //     if (action == null || action.isEmpty()) {
-        //         index(req, res);
-        //     } else if ("add".equalsIgnoreCase(action)) {
-        //         create(req, res);
-        //     } else if ("list".equalsIgnoreCase(action)) {
-        //       index(req, res);
-        //     } else if ("edit".equalsIgnoreCase(action)) {
-        //         edit(req,res);
-        //     }
-    }
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String action = req.getParameter("action");
+		if ("create".equals(action)) {
+			createP(req, res);
+		} else if ("update".equals(action)) {
+			update(req, res);
+		} else if ("delete".equals(action)) {
+			delete(req, res);
+		}
 
+	}
 
+	// GET REQUEST
+	protected void index(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        String action = req.getParameter("action");
-            if ("create".equals(action)) {
-                createP(req, res);
-            } else if ("update".equals(action)) {
-               update(req,res);
-            } else if ("delete".equals(action)) {
-                delete(req,res);
-            }
-       
-    }
-
-    // GET REQUEST 
-    protected void index(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
 		// req.setAttribute("model", model);
 		this.getServletContext().getRequestDispatcher("/views/article/index.jsp").forward(req, res);
 
 	}
 
-    protected void create(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	protected void create(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		this.getServletContext().getRequestDispatcher("/views/member/add.jsp").forward(req, res);
 	}
 
-    protected void edit(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        String memberId = req.getParameter("id");
+	protected void edit(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String memberId = req.getParameter("id");
 
-        
-    }
+	}
 
-    protected void createP(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-  
-    }
-    protected void delete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-       
-    }
+	protected void createP(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-    protected void update(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
-    {
-     
-    }
+	}
+
+	protected void delete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+	}
+
+	protected void update(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+	}
 }
